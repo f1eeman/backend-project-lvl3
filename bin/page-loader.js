25804c958f8f20065e1db0d011ac3b9cbbae950f
f1eeman <mr.f1eeman@gmail.com>
2020-11-30 16:02:54 +0600

    refactor: change logic of bin program, change logic of buildAssetName func

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -4,38 +4,34 @@
 import program from 'commander';
 import downloadPage from '../src/index.js';
 
-const pageLoaderCli = async () => {
-  program
-    .version('0.0.1')
-    .description('Downloads pages from the Internet and saves them to your computer')
-    .option('-o, --output [path]', 'directory for data downloading', `${process.cwd()}`)
-    .arguments('<address>')
-    .action(async (address) => {
-      if (program.output === process.cwd()) {
-        return downloadPage(address, program.output);
-      }
-      return downloadPage(address, `${process.cwd()}${program.output}`);
-    });
-  return program.parseAsync(process.argv).then(() => {
-    // eslint-disable-next-line no-underscore-dangle
-    const [result] = program._actionResults;
-    return result;
+program
+  .version('0.0.1')
+  .description('Downloads pages from the Internet and saves them to your computer')
+  .option('-o, --output [path]', 'directory for data downloading', `${process.cwd()}`)
+  .arguments('<address>')
+  .action(async (address) => {
+    if (program.output === process.cwd()) {
+      return downloadPage(address, program.output);
+    }
+    return downloadPage(address, `${process.cwd()}${program.output}`);
   });
-};
 
-pageLoaderCli()
-  // eslint-disable-next-line no-console
+program.parseAsync(process.argv).then(() => {
+  // eslint-disable-next-line no-underscore-dangle
+  const [result] = program._actionResults;
+  return result;
+})
   .then((fileName) => console.log(`\nPage was downloaded as '${fileName}'`))
   .then(() => process.exit(0))
   .catch((error) => {
     console.error('Oops! Something went wrong');
-    if (error.code) {
+    if (error.isAxiosError) {
+      const { response } = error;
+      console.log('status-code: ', response.status);
+      console.log('status-text: ', response.statusText);
+      console.log('error.message: ', error.message);
+    } else {
       console.error(error);
-      return;
     }
-    const { response } = error;
-    console.log('status-code: ', response.status);
-    console.log('status-text: ', response.statusText);
-    console.log('The requested page is unavailable or does not exist. Check that the address is correct');
     process.exit(1);
   });
--- src/index.js
+++ src/index.js
@@ -7,32 +7,23 @@ import debug from 'debug';
 import 'axios-debug-log';
 
 const log = debug('page-loader:');
-
-const getPath = (catalog, fileName) => path.join(catalog, fileName);
+const noop = () => {};
 
 const buildName = (link) => {
   const { pathname, host } = new URL(link);
   const fileName = `${host}${pathname}`
-    .split(/[^A-Za-z0-9]/)
-    .filter((el) => el !== '')
+    .split(/[^\w+]/gi)
     .join('-');
   return fileName;
 };
 
 const buildAssetName = (rootAddress, link) => {
-  const { pathname, host } = new URL(link, rootAddress);
-  const name = `${host}/${pathname}`
-    .split(/[^A-Za-z0-9]/)
-    .filter((el) => el !== '')
-    .join('-');
-  const regexp = /.+(jpg|jpeg|svg|webp|png|gif|ico|css|js)/;
-  const [assetName] = name.match(regexp) || [name.concat('-html')];
-  const indexLastDash = assetName.lastIndexOf('-');
-  const newName = `${assetName.slice(0, indexLastDash)}.${assetName.slice(indexLastDash + 1)}`;
-  return newName;
+  const extName = path.extname(link);
+  const [linkWithoutExtName] = link.match(new RegExp(`.+(?=${extName})`));
+  const name = buildName(new URL(linkWithoutExtName, rootAddress));
+  const nameWithExtName = name.concat(extName || '.html');
+  return nameWithExtName;
 };
-
-const modifyName = (name, value) => name.concat(value);
 
 const getPageData = (html, rootAddress, assetsDirectoryName) => {
   const links = [];
@@ -47,47 +38,22 @@ const getPageData = (html, rootAddress, assetsDirectoryName) => {
   };
   const { host: rootHost } = new URL(rootAddress);
 
-  // Object.entries(mapping)
-  //   .forEach(([tagName, attributeName]) => {
-  //     const elements = $(tagName).toArray();
-  //     elements
-  //       .map(({ attribs }, index) => attribs[attributeName])
-  //       .map((link) => {
-  //         // console.log('maaaaaaaaaaaaaaaap Link', link);
-  //         return new URL(link, rootAddress);
-  //       })
-  //       .filter(({ host }) => {
-  //         return host === rootHost;
-  //       })
-  //       .map(({ href }) => href)
-  //       .forEach((link) => {
-  //         // console.log('!!!!!!!!!!!!link', link);
-  //         links.push(link);
-  //         const assetName = buildAssetName(rootAddress, link);
-  //         $(tagName).attr(attributeName, path.join(assetsDirectoryName, assetName));
-  //       });
-  //   });
-
   Object.entries(mapping)
     .forEach(([tagName, attributeName]) => {
       const elements = $(tagName).toArray();
       elements
         .map(({ attribs }, index) => ({ link: attribs[attributeName], index }))
         .map(({ link, index }) => {
-          // console.log('maaaaaaaaaaaaaaaap Link', link);
           const { host, href } = new URL(link, rootAddress);
           return { href, host, index };
         })
         .filter(({ host }) => host === rootHost)
         .forEach(({ href, index }) => {
-          // console.log('!!!!!!!!!!!!link', link);
           links.push(href);
           const assetName = buildAssetName(rootAddress, href);
           $(elements[index]).attr(attributeName, path.join(assetsDirectoryName, assetName));
         });
     });
-
-  // console.log('links', links);
 
   return { html: $.html(), links };
 };
@@ -95,25 +61,25 @@ const getPageData = (html, rootAddress, assetsDirectoryName) => {
 const downloadAsset = (link, directoryPath, assetName) => axios
   .get(link, { responseType: 'arraybuffer' })
   .then(({ data }) => {
-    const assetPath = getPath(directoryPath, assetName);
+    const assetPath = path.join(directoryPath, assetName);
     return fsp.writeFile(assetPath, data);
   });
 
-const downloadPage = (address, outputDirectory) => {
+const downloadPage = (address, outputDirectory = process.cwd()) => {
   const rootName = buildName(address);
-  const htmlExtension = '.html';
-  const htmlName = modifyName(rootName, htmlExtension);
-  const htmlPath = getPath(outputDirectory, htmlName);
+  const fileExtension = '.html';
+  const fileName = rootName.concat(fileExtension);
+  const filePath = path.join(outputDirectory, fileName);
   const assetsDirectoryNamePostfix = '_files';
-  const assetsDirectoryName = modifyName(rootName, assetsDirectoryNamePostfix);
-  const assetsDirectoryPath = getPath(outputDirectory, assetsDirectoryName);
+  const assetsDirectoryName = rootName.concat(assetsDirectoryNamePostfix);
+  const assetsDirectoryPath = path.join(outputDirectory, assetsDirectoryName);
   let pageData;
 
   return axios.get(address)
     .then(({ data }) => {
       pageData = getPageData(data, address, assetsDirectoryName);
-      log(`creating an html file: ${htmlPath}`);
-      return fsp.writeFile(htmlPath, pageData.html);
+      log(`creating an html file: ${filePath}`);
+      return fsp.writeFile(filePath, pageData.html);
     })
     .then(() => {
       log(`creating a directory for web page assets: ${assetsDirectoryPath}`);
@@ -124,13 +90,13 @@ const downloadPage = (address, outputDirectory) => {
         title: link,
         task: () => {
           const assetName = buildAssetName(address, link);
-          return downloadAsset(link, assetsDirectoryPath, assetName);
+          return downloadAsset(link, assetsDirectoryPath, assetName).catch(noop);
         },
       }));
       const listr = new Listr(tasks, { concurrent: true });
       return listr.run();
     })
-    .then(() => htmlName);
+    .then(() => fileName);
 };
 
 export default downloadPage;
